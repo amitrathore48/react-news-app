c2ebcaeda823efcefa1672b53427d2ebf4c8902c
Amit Rathore <amitrathore48@gmail.com>
2023-05-25 19:38:39 +0530

    Initial code commit and push

---
+++ src/components/Loading.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import loading from "./loading.gif";
+
+const Loading = () => {
+  return (
+    <div className="text-center">
+      <img src={loading} alt="loading" style={{width: "60px"}} />
+    </div>
+  );
+};
+
+export default Loading;
